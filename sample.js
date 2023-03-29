646479fbf7e05c6d2eabdee99d358724b823c01f
tapeinooo <117516592+tapeinooo@users.noreply.github.com>
2023-03-29 20:45:45 +0900

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,7 @@
+function initMap() {
+  var opts = {
+    zoom: 15,
+    center: new google.maps.LatLng(35.1239654, 136.9417741)
+  };
+  var map = new google.maps.Map(document.getElementById("map"), opts);
+}
