aaee098abdeee24de0580518e4261c348bbf57af
tapeinooo <117516592+tapeinooo@users.noreply.github.com>
2023-03-29 21:04:17 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 function initMap() {
   var opts = {
     zoom: 15,
-    center: new google.maps.LatLng(35.1239654, 136.9417741)
+    center: new google.maps.LatLng(35.6807527,139.7670716)
   };
   var map = new google.maps.Map(document.getElementById("map"), opts);
 }
