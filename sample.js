7d932af6d3c3c9ca07cf5c90e508e2c0b2d23e41
tapeinooo <117516592+tapeinooo@users.noreply.github.com>
2023-03-29 21:14:13 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,7 +1,15 @@
 function initMap() {
+  var useragent = navigator.userAgent;
+  var mapdiv = document.getElementById("map");
+
+  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1){
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '100%';
+  }
+
   var opts = {
     zoom: 15,
     center: new google.maps.LatLng(35.6807527,139.7670716)
   };
-  var map = new google.maps.Map(document.getElementById("map"), opts);
+  var map = new google.maps.Map(mapdiv, opts);
 }
